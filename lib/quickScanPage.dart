8c81fec6e761aa2227048c3749c6a62a6cc5a1e3
Ravisha2020 <ravisha.20200482@iit.ac.lk>
2022-03-24 19:51:27 +0530

    Display disease details

--- lib/quickScanPage.dart
+++ lib/quickScanPage.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+// import 'package:geolocator/geolocator.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tflite/tflite.dart';
 import 'package:image/image.dart' as img;
@@ -19,9 +19,72 @@ class _QuickScanPageState extends State<QuickScanPage> {
   String result = '';
   ImagePicker? imagePicker;
 
-  var _latitude = "";
-  var _longitude = "";
-  var _altitude = "";
+  String _solution = "";
+
+  final String _beetle = """
+      Mechanical Solution:
+  
+  Beetle can be removed using a
+  pointed metal hook.
+  
+      Cultural Solution:
+  
+  Decaying coconut logs and
+  stumps should be removed and
+  burnt. Breeding media such as
+  manure heaps and coir dust
+  heaps should be disposed or
+  earthed up with the soil.
+  
+      Chemical Solution:
+  
+  Place naphthalene balls into
+  each of the innermost leaf
+  axis.""";
+
+  final String _mite = """
+  - If reported only in a few
+  palms, cut and burn all the
+  nuts in the infested palms.
+  
+  - For infestations in boundary
+  areas or heavily infested
+  palms, apply 30% used engine
+  oil on nuts of infested palms.
+  Apply this mixture at 2 monthly
+  intervals until the damage is
+  reduced.
+  
+  - Apply palm / vegetable oil
+  and Sulphur mixture. This
+  mixture can be sprayed from
+  ground level using a modified
+  knapsack sprayer of a power
+  sprayer""";
+
+  final String _budRot = """
+  - For infected young palms in
+  an advanced stage, the crown
+  should be cut and burnt to
+  destroy the fungus.
+  
+  - If detected early, the bud
+  region should be thoroughly
+  wetted with Bordeaux mixture
+  or 1% Copper fungicide
+  solution.
+  
+  - For older bearing palms,
+  wet the bud region with the
+  recommended chemical to kill
+  the fungus.""";
+
+  final String _fresh = """
+    No Diseases Detected...""";
+
+  // var _latitude = "";
+  // var _longitude = "";
+  // var _altitude = "";
 
   void _getFromCamera() async {
     XFile? pickedFile = await imagePicker!.pickImage(
@@ -29,10 +92,21 @@ class _QuickScanPageState extends State<QuickScanPage> {
       maxHeight: 1080,
       maxWidth: 1080,
     );
-    setState(() {
-      _getLocation();
+    setState(() async {
+      // await _getLocation();
       imageFile = File(pickedFile!.path);
-      imageClassification(imageFile!);
+      await imageClassification(imageFile!);
+
+      if (result.contains("Beetle")) {
+        _solution = _beetle;
+      } else if (result.contains("Coconut Mite")) {
+        _solution = _mite;
+      } else if (result.contains("Bud Rot")) {
+        _solution = _budRot;
+      } else if (result.contains("Fresh")) {
+        _solution = _fresh;
+      }
+
       popupModel(context, imageFile);
     });
   }
@@ -49,7 +123,7 @@ class _QuickScanPageState extends State<QuickScanPage> {
     int startTime = DateTime.now().millisecondsSinceEpoch;
     var recognitions = await Tflite.runModelOnImage(
       path: image.path,
-      numResults: 2,
+      numResults: 1,
     );
     print(recognitions!.length.toString());
     setState(() {
@@ -58,7 +132,7 @@ class _QuickScanPageState extends State<QuickScanPage> {
     recognitions.forEach((element) {
       setState(() {
         print(element.toString());
-        result += element['label'] + '   ' + (element['confidence'] as double).toStringAsFixed(2) + '\n';
+        result += element['label'] + ': ' + (element['confidence'] as double).toStringAsFixed(2) + '\n';
       });
     });
     int endTime = DateTime.now().millisecondsSinceEpoch;
@@ -85,43 +159,43 @@ class _QuickScanPageState extends State<QuickScanPage> {
     super.initState();
     imagePicker = ImagePicker();
     loadDataModelFiles();
-    _getLocation();
+    // _getLocation();
   }
 
-  Future<void> _getLocation() async {
-    Position position = await _determinePosition();
+  // Future<void> _getLocation() async {
+  //   Position position = await _determinePosition();
+  //
+  //   setState(() {
+  //     _latitude = position.latitude.toString();
+  //     _longitude = position.longitude.toString();
+  //     _altitude = position.altitude.toString();
+  //   });
+  // }
 
-    setState(() {
-      _latitude = position.latitude.toString();
-      _longitude = position.longitude.toString();
-      _altitude = position.altitude.toString();
-    });
-  }
-
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    return await Geolocator.getCurrentPosition();
-  }
+  // Future<Position> _determinePosition() async {
+  //   bool serviceEnabled;
+  //   LocationPermission permission;
+  //
+  //   serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  //   if (!serviceEnabled) {
+  //     return Future.error('Location services are disabled.');
+  //   }
+  //
+  //   permission = await Geolocator.checkPermission();
+  //   if (permission == LocationPermission.denied) {
+  //     permission = await Geolocator.requestPermission();
+  //     if (permission == LocationPermission.denied) {
+  //       return Future.error('Location permissions are denied');
+  //     }
+  //   }
+  //
+  //   if (permission == LocationPermission.deniedForever) {
+  //     return Future.error(
+  //         'Location permissions are permanently denied, we cannot request permissions.');
+  //   }
+  //
+  //   return await Geolocator.getCurrentPosition();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -130,10 +204,10 @@ class _QuickScanPageState extends State<QuickScanPage> {
         child: ElevatedButton(
             child: const Text('Capture Image'),
             onPressed: () {
-              _getLocation();
-              if (_latitude != "") {
-                _getFromCamera();
-              }
+              // _getLocation();
+              // if (_latitude != "") {
+              _getFromCamera();
+              // }
             },
             style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all(Colors.blue),
@@ -155,46 +229,43 @@ class _QuickScanPageState extends State<QuickScanPage> {
                 borderRadius: BorderRadius.vertical(top: Radius.circular(15))),
             height: MediaQuery.of(context).size.height * .95,
             child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+              child: Row(
                 children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Result\n',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 30.0,
+                        ),
+                      ),
+                      Text(
+                        result + '\n',
+                        style: const TextStyle(
+                          color: Colors.red,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Image.file(
+                          file,
+                          height: 150.0,
+                        ),
+                      ),
+                    ],
+                  ),
                   Text(
-                    'Latitude:  ' + _latitude,
+                    _solution,
                     style: const TextStyle(
                       color: Colors.black,
                       fontSize: 15.0,
                     ),
                   ),
-                  Text(
-                    'Longitude:  ' + _longitude,
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 15.0,
-                    ),
-                  ),
-                  Text(
-                    'Altitude:  ' + _altitude,
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 15.0,
-                    ),
-                  ),
-                  Text(
-                    result,
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 15.0,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Image.file(
-                      file,
-                      height: 200.0,
-                    ),
-                  )
                 ],
-              ),
+              )
             ),
           );
         });
